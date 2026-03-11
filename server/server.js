7ae869ee30e80583b5ca96787c7017f5fdcca9e3
Alana Prem <alanaprem001@gmail.com>
2026-03-11 12:25:19 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const allowedOrigins =['http://localhost:5173']
 //Middleware configuration
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin: allowedOrigins, credentials:true}))
+app.use(cors({origin: "https://grocery-delivery-c1yzilvg7-alana-prem01s-projects.vercel.app/", credentials:true}))
 
 app.get('/',(req,res) =>res.send("API is Working"))
 app.use('/api/user',userRouter)
